okuoku/WebKit

commit 5e28301049338fa1ed75db9dd58afb17114fb5a2

REGRESSION (r232416): Can not scroll after swiping back on quoteunquoteapps.com https://bugs.webkit.org/show_bug.cgi?id=190377 <rdar://problem/45108222>
Reviewed by Andy Estes.

Introduce the notion of 'pausing' to SnapshotRemovalTracker.
Reimplement r232416 in terms of this: the SnapshotRemovalTracker
starts out paused (not accepting events), and un-pauses when
we get a provisional load or same-document navigation.
This way, we don't lose the watchdog timer in cases where we get
no provisional load, same-document navigation, or main frame load
(which is the separate root cause for this bug -- this just papers
over it with a timeout).

* UIProcess/Cocoa/ViewGestureController.cpp:
(WebKit::ViewGestureController::didStartProvisionalLoadForMainFrame):
(WebKit::ViewGestureController::didSameDocumentNavigationForMainFrame):
Resume the snapshot removal tracker.

(WebKit::ViewGestureController::didReachMainFrameLoadTerminalState):
If we didn't see a provisional load or same document navigation,
but somehow got to the terminal load state, immediately remove the snapshot.

(WebKit::ViewGestureController::SnapshotRemovalTracker::resume):
(WebKit::ViewGestureController::SnapshotRemovalTracker::start):
Start the SnapshotRemovalTracker out in the paused state; it will be
resumed in the same places we previously would call the
provisionalOrSameDocumentLoadCallback.

(WebKit::ViewGestureController::SnapshotRemovalTracker::stopWaitingForEvent):
Ignore (but debug log) incoming events while paused.

* UIProcess/Cocoa/ViewGestureController.h:
(WebKit::ViewGestureController::SnapshotRemovalTracker::pause):
(WebKit::ViewGestureController::SnapshotRemovalTracker::isPaused const):
Add the pausing bit.

* UIProcess/ios/ViewGestureControllerIOS.mm:
(WebKit::ViewGestureController::endSwipeGesture):
* UIProcess/mac/ViewGestureControllerMac.mm:
(WebKit::ViewGestureController::endSwipeGesture):
Remove m_provisionalOrSameDocumentLoadCallback.

Source/WebKit/UIProcess/Cocoa/ViewGestureController.cpp
@@ -129,10 +129,8 @@ bool ViewGestureController::canSwipeInDirection(SwipeDirection direction) const
 
 void ViewGestureController::didStartProvisionalLoadForMainFrame()
 {
-    if (auto provisionalOrSameDocumentLoadCallback = WTFMove(m_provisionalOrSameDocumentLoadCallback))
-        provisionalOrSameDocumentLoadCallback();
+    m_snapshotRemovalTracker.resume();
 }
-
 
 void ViewGestureController::didFirstVisuallyNonEmptyLayoutForMainFrame()
 {
@@ -161,8 +159,7 @@ void ViewGestureController::didRestoreScrollPosition()
 
 void ViewGestureController::didReachMainFrameLoadTerminalState()
 {
-    if (m_provisionalOrSameDocumentLoadCallback) {
-        m_provisionalOrSameDocumentLoadCallback = nullptr;
+    if (m_snapshotRemovalTracker.isPaused()) {
         removeSwipeSnapshot();
         return;
     }
@@ -191,9 +188,7 @@ void ViewGestureController::didReachMainFrameLoadTerminalState()
 
 void ViewGestureController::didSameDocumentNavigationForMainFrame(SameDocumentNavigationType type)
 {
-
-    if (auto provisionalOrSameDocumentLoadCallback = WTFMove(m_provisionalOrSameDocumentLoadCallback))
-        provisionalOrSameDocumentLoadCallback();
+    m_snapshotRemovalTracker.resume();
 
     bool cancelledOutstandingEvent = false;
 
@@ -260,6 +255,13 @@ void ViewGestureController::SnapshotRemovalTracker::log(const String& log) const
 #endif
     LOG(ViewGestures, "Swipe Snapshot Removal (%0.2f ms) - %s", sinceStart.milliseconds(), log.utf8().data());
 }
+    
+void ViewGestureController::SnapshotRemovalTracker::resume()
+{
+    if (isPaused() && m_outstandingEvents)
+        log("resume");
+    m_paused = false;
+}
 
 void ViewGestureController::SnapshotRemovalTracker::start(Events desiredEvents, WTF::Function<void()>&& removalCallback)
 {
@@ -270,6 +272,10 @@ void ViewGestureController::SnapshotRemovalTracker::start(Events desiredEvents, 
     log("start");
 
     startWatchdog(swipeSnapshotRemovalWatchdogDuration);
+    
+    // Initially start out paused; we'll resume when the load is committed.
+    // This avoids processing callbacks from earlier loads.
+    pause();
 }
 
 void ViewGestureController::SnapshotRemovalTracker::reset()
@@ -287,6 +293,11 @@ bool ViewGestureController::SnapshotRemovalTracker::stopWaitingForEvent(Events e
 
     if (!(m_outstandingEvents & event))
         return false;
+
+    if (isPaused()) {
+        log("is paused; ignoring event: " + eventsDescription(event));
+        return false;
+    }
 
     log(logReason + eventsDescription(event));
 

Source/WebKit/UIProcess/Cocoa/ViewGestureController.h
@@ -169,6 +169,10 @@ private:
 
         void start(Events, WTF::Function<void()>&&);
         void reset();
+        
+        void pause() { m_paused = true; }
+        void resume();
+        bool isPaused() const { return m_paused; }
 
         bool eventOccurred(Events);
         bool cancelOutstandingEvent(Events);
@@ -190,6 +194,8 @@ private:
         MonotonicTime m_startTime;
 
         RunLoop::Timer<SnapshotRemovalTracker> m_watchdogTimer;
+        
+        bool m_paused { true };
     };
 
 #if PLATFORM(MAC)
@@ -301,7 +307,6 @@ private:
     uint64_t m_snapshotRemovalTargetRenderTreeSize { 0 };
 #endif
 
-    WTF::Function<void()> m_provisionalOrSameDocumentLoadCallback;
     SnapshotRemovalTracker m_snapshotRemovalTracker;
 };
 

Source/WebKit/UIProcess/ios/ViewGestureControllerIOS.mm
@@ -300,29 +300,27 @@ void ViewGestureController::endSwipeGesture(WebBackForwardListItem* targetItem, 
         return;
     }
 
-    m_provisionalOrSameDocumentLoadCallback = [this] {
-        if (auto drawingArea = m_webPageProxy.drawingArea()) {
-            uint64_t pageID = m_webPageProxy.pageID();
-            GestureID gestureID = m_currentGestureID;
-            drawingArea->dispatchAfterEnsuringDrawing([pageID, gestureID] (CallbackBase::Error error) {
-                if (auto gestureController = controllerForGesture(pageID, gestureID))
-                    gestureController->willCommitPostSwipeTransitionLayerTree(error == CallbackBase::Error::None);
-            });
-            drawingArea->hideContentUntilPendingUpdate();
-        } else {
-            removeSwipeSnapshot();
-            return;
-        }
-
-        // FIXME: Should we wait for VisuallyNonEmptyLayout like we do on Mac?
-        m_snapshotRemovalTracker.start(SnapshotRemovalTracker::RenderTreeSizeThreshold
-            | SnapshotRemovalTracker::RepaintAfterNavigation
-            | SnapshotRemovalTracker::MainFrameLoad
-            | SnapshotRemovalTracker::SubresourceLoads
-            | SnapshotRemovalTracker::ScrollPositionRestoration, [this] {
-                this->removeSwipeSnapshot();
+    if (auto drawingArea = m_webPageProxy.drawingArea()) {
+        uint64_t pageID = m_webPageProxy.pageID();
+        GestureID gestureID = m_currentGestureID;
+        drawingArea->dispatchAfterEnsuringDrawing([pageID, gestureID] (CallbackBase::Error error) {
+            if (auto gestureController = controllerForGesture(pageID, gestureID))
+                gestureController->willCommitPostSwipeTransitionLayerTree(error == CallbackBase::Error::None);
         });
-    };
+        drawingArea->hideContentUntilPendingUpdate();
+    } else {
+        removeSwipeSnapshot();
+        return;
+    }
+
+    // FIXME: Should we wait for VisuallyNonEmptyLayout like we do on Mac?
+    m_snapshotRemovalTracker.start(SnapshotRemovalTracker::RenderTreeSizeThreshold
+        | SnapshotRemovalTracker::RepaintAfterNavigation
+        | SnapshotRemovalTracker::MainFrameLoad
+        | SnapshotRemovalTracker::SubresourceLoads
+        | SnapshotRemovalTracker::ScrollPositionRestoration, [this] {
+            this->removeSwipeSnapshot();
+    });
 
     if (ViewSnapshot* snapshot = targetItem->snapshot()) {
         m_backgroundColorForCurrentSnapshot = snapshot->backgroundColor();

Source/WebKit/UIProcess/mac/ViewGestureControllerMac.mm
@@ -743,15 +743,13 @@ void ViewGestureController::endSwipeGesture(WebBackForwardListItem* targetItem, 
     m_webPageProxy.navigationGestureDidEnd(true, *targetItem);
     m_webPageProxy.goToBackForwardItem(*targetItem);
 
-    m_provisionalOrSameDocumentLoadCallback = [this, renderTreeSize] {
-        SnapshotRemovalTracker::Events desiredEvents = SnapshotRemovalTracker::VisuallyNonEmptyLayout
-            | SnapshotRemovalTracker::MainFrameLoad
-            | SnapshotRemovalTracker::SubresourceLoads
-            | SnapshotRemovalTracker::ScrollPositionRestoration;
-        if (renderTreeSize)
-            desiredEvents |= SnapshotRemovalTracker::RenderTreeSizeThreshold;
-        m_snapshotRemovalTracker.start(desiredEvents, [this] { this->forceRepaintIfNeeded(); });
-    };
+    SnapshotRemovalTracker::Events desiredEvents = SnapshotRemovalTracker::VisuallyNonEmptyLayout
+        | SnapshotRemovalTracker::MainFrameLoad
+        | SnapshotRemovalTracker::SubresourceLoads
+        | SnapshotRemovalTracker::ScrollPositionRestoration;
+    if (renderTreeSize)
+        desiredEvents |= SnapshotRemovalTracker::RenderTreeSizeThreshold;
+    m_snapshotRemovalTracker.start(desiredEvents, [this] { this->forceRepaintIfNeeded(); });
 
     // FIXME: Like on iOS, we should ensure that even if one of the timeouts fires,
     // we never show the old page content, instead showing the snapshot background color.

commit 87b186370a731f7a953eda8e299ee054c196217b

Cherry-pick r240772. rdar://problem/47774490
    iOS: Crash in InteractiveUpdateHandler set by ViewGestureController::beginSwipeGesture
    https://bugs.webkit.org/show_bug.cgi?id=194083

    Reviewed by Tim Horton.

    We think this crash is a regression from r236966. Prior to r236966, we could only called
    removeSwipeSnapshot() only when m_provisionalOrSameDocumentLoadCallback was set but now
    we can call it either when m_snapshotRemovalTracker::start was called, or it had been reset.
    This can result in m_webPageProxyForBackForwardListForCurrentSwipe getting cleared before
    InteractiveUpdateHandler is called by UIGestureRecognizer, resulting in the crash.

    This patch tries to restore the behavior prior to r236966 by only invoking removeSwipeSnapshot()
    when SnapshotRemovalTracker has a valid removal callback set.

    Unfortunately no new tests since there is no reproducible test case, and neither API tests
    nor layout tests seem to have the capability to trigger swipe gestures via UIGestureRecognizer,
    which is required for this crash to occur. Notably, back-forward swipe tests I enabled in
    r240765 bypass UIKit and emulates the action instead.

    * UIProcess/Cocoa/ViewGestureController.cpp:
    (WebKit::ViewGestureController::didReachMainFrameLoadTerminalState):
    * UIProcess/Cocoa/ViewGestureController.h:
    (WebKit::ViewGestureController::SnapshotRemovalTracker::hasRemovalCallback const):

    Canonical link: https://commits.webkit.org/208553@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@240772 268f45cc-cd09-0410-ab3c-d52691b4dbfc

git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-607-branch@241072 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/Cocoa/ViewGestureController.cpp
@@ -188,7 +188,7 @@ void ViewGestureController::didRestoreScrollPosition()
 
 void ViewGestureController::didReachMainFrameLoadTerminalState()
 {
-    if (m_snapshotRemovalTracker.isPaused()) {
+    if (m_snapshotRemovalTracker.isPaused() && m_snapshotRemovalTracker.hasRemovalCallback()) {
         removeSwipeSnapshot();
         return;
     }

Source/WebKit/UIProcess/Cocoa/ViewGestureController.h
@@ -179,6 +179,7 @@ private:
         void pause() { m_paused = true; }
         void resume();
         bool isPaused() const { return m_paused; }
+        bool hasRemovalCallback() const { return !!m_removalCallback; }
 
         bool eventOccurred(Events);
         bool cancelOutstandingEvent(Events);