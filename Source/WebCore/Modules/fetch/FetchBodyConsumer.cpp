okuoku/WebKit

commit 60a5e100aa0e2ad4b6e32102d732d76f664ffcb1

Cherry-pick r266087. rdar://problem/67836301
    Implement Request/Response consuming as FormData
    https://bugs.webkit.org/show_bug.cgi?id=215671

    Patch by Alex Christensen <achristensen@webkit.org> on 2020-08-24
    Reviewed by Darin Adler.

    LayoutTests/imported/w3c:

    * web-platform-tests/fetch/api/abort/general.any-expected.txt:
    * web-platform-tests/fetch/api/abort/general.any.worker-expected.txt:
    * web-platform-tests/fetch/api/request/request-consume-empty-expected.txt:
    This remaining failing test now fails similarly in all browsers.
    * web-platform-tests/fetch/api/request/request-consume-expected.txt:
    * web-platform-tests/fetch/api/request/request-init-002-expected.txt:
    * web-platform-tests/fetch/api/response/response-consume-empty-expected.txt:
    This remaining failing test now fails similarly in all browsers.
    * web-platform-tests/fetch/api/response/response-consume-expected.txt:
    * web-platform-tests/fetch/api/response/response-error-from-stream-expected.txt:
    This change makes the formData failures in this file look like all the other failures in this file,
    which should be fixed together in a separate patch.
    * web-platform-tests/fetch/api/response/response-init-002-expected.txt:
    * web-platform-tests/url/urlencoded-parser.any-expected.txt:
    * web-platform-tests/url/urlencoded-parser.any.worker-expected.txt:
    * web-platform-tests/service-workers/service-worker/fetch-event-respond-with-custom-response.https-expected.txt:

    Source/WebCore:

    Covered by many newly passing WPT tests, for most of which Safari was the only failing browser.

    * Modules/fetch/FetchBody.cpp:
    (WebCore::FetchBody::formData):
    (WebCore::FetchBody::consume):
    (WebCore::FetchBody::consumeFormData):
    * Modules/fetch/FetchBody.h:
    * Modules/fetch/FetchBodyConsumer.cpp:
    (WebCore::formDataFromData):
    (WebCore::resolveWithTypeAndData):
    (WebCore::FetchBodyConsumer::resolve):

Source/WebCore/Modules/fetch/FetchBodyConsumer.cpp
@@ -29,9 +29,12 @@
 #include "config.h"
 #include "FetchBodyConsumer.h"
 
+#include "HTTPHeaderField.h"
 #include "JSBlob.h"
 #include "ReadableStreamChunk.h"
 #include "TextResourceDecoder.h"
+#include <wtf/StringExtras.h>
+#include <wtf/URLParser.h>
 
 namespace WebCore {
 
@@ -39,7 +42,173 @@ static inline Ref<Blob> blobFromData(const unsigned char* data, unsigned length,
 {
     Vector<uint8_t> value(length);
     memcpy(value.data(), data, length);
-    return Blob::create(WTFMove(value), contentType);
+    return Blob::create(WTFMove(value), Blob::normalizedContentType(contentType));
+}
+
+// https://mimesniff.spec.whatwg.org/#http-quoted-string-token-code-point
+static bool isHTTPQuotedStringTokenCodePoint(UChar c)
+{
+    return c == 0x09
+        || (c >= 0x20 && c <= 0x7E)
+        || (c >= 0x80 && c <= 0xFF);
+}
+
+struct MimeType {
+    String type;
+    String subtype;
+    HashMap<String, String> parameters;
+};
+
+static HashMap<String, String> parseParameters(StringView input, size_t position)
+{
+    HashMap<String, String> parameters;
+    while (position < input.length()) {
+        while (position < input.length() && RFC7230::isWhitespace(input[position]))
+            position++;
+        size_t nameBegin = position;
+        while (position < input.length() && input[position] != '=' && input[position] != ';')
+            position++;
+        if (position >= input.length())
+            break;
+        if (input[position] == ';') {
+            position++;
+            continue;
+        }
+        StringView parameterName = input.substring(nameBegin, position - nameBegin);
+        position++;
+        if (position >= input.length())
+            break;
+
+        StringView parameterValue;
+        if (position < input.length() && input[position] == '"') {
+            position++;
+            size_t valueBegin = position;
+            while (position < input.length() && input[position] != '"')
+                position++;
+            parameterValue = input.substring(valueBegin, position - valueBegin);
+            position++;
+        } else {
+            size_t valueBegin = position;
+            while (position < input.length() && input[position] != ';')
+                position++;
+            parameterValue = stripLeadingAndTrailingHTTPSpaces(input.substring(valueBegin, position - valueBegin));
+        }
+
+        if (parameterName.length()
+            && isValidHTTPToken(parameterName)
+            && parameterValue.isAllSpecialCharacters<isHTTPQuotedStringTokenCodePoint>()) {
+            String nameString = parameterName.toString();
+            if (!parameters.contains(nameString))
+                parameters.set(nameString, parameterValue.toString());
+        }
+    }
+    return parameters;
+}
+
+// https://mimesniff.spec.whatwg.org/#parsing-a-mime-type
+static Optional<MimeType> parseMIMEType(const String& contentType)
+{
+    String input = stripLeadingAndTrailingHTTPSpaces(contentType);
+    size_t slashIndex = input.find('/');
+    if (slashIndex == notFound)
+        return WTF::nullopt;
+
+    String type = input.substring(0, slashIndex);
+    if (!type.length() || !isValidHTTPToken(type))
+        return WTF::nullopt;
+    
+    size_t semicolonIndex = input.find(';', slashIndex);
+    String subtype = stripLeadingAndTrailingHTTPSpaces(input.substring(slashIndex + 1, semicolonIndex - slashIndex - 1));
+    if (!subtype.length() || !isValidHTTPToken(subtype))
+        return WTF::nullopt;
+
+    return {{ WTFMove(type), WTFMove(subtype), parseParameters(StringView(input), semicolonIndex + 1) }};
+}
+
+// https://fetch.spec.whatwg.org/#concept-body-package-data
+static RefPtr<DOMFormData> packageFormData(const String& contentType, const char* data, size_t length)
+{
+    auto parseMultipartPart = [] (const char* part, size_t partLength, DOMFormData& form) -> bool {
+        const char* headerEnd = static_cast<const char*>(memmem(part, partLength, "\r\n\r\n", 4));
+        if (!headerEnd)
+            return false;
+        const char* headerBegin = part;
+        size_t headerLength = headerEnd - headerBegin;
+
+        const char* bodyBegin = headerEnd + strlen("\r\n\r\n");
+        size_t bodyLength = partLength - (bodyBegin - headerBegin);
+
+        String header = String::fromUTF8(headerBegin, headerLength);
+
+        const char* contentDispositionCharacters = "Content-Disposition:";
+        size_t contentDispositionBegin = header.find(contentDispositionCharacters);
+        if (contentDispositionBegin == notFound)
+            return false;
+        size_t contentDispositionEnd = header.find("\r\n", contentDispositionBegin);
+        size_t contentDispositionParametersBegin = header.find(';', contentDispositionBegin + strlen(contentDispositionCharacters));
+        if (contentDispositionParametersBegin != notFound)
+            contentDispositionParametersBegin++;
+
+        auto parameters = parseParameters(StringView(header).substring(contentDispositionParametersBegin, contentDispositionEnd - contentDispositionParametersBegin), 0);
+        String name = parameters.get("name");
+        if (!name)
+            return false;
+        String filename = parameters.get("filename");
+        if (!filename)
+            form.append(name, String::fromUTF8(bodyBegin, bodyLength));
+        else {
+            String contentType = "text/plain"_s;
+
+            const char* contentTypeCharacters = "Content-Type:";
+            size_t contentTypePrefixLength = strlen(contentTypeCharacters);
+            size_t contentTypeBegin = header.find(contentTypeCharacters);
+            if (contentTypeBegin != notFound) {
+                size_t contentTypeEnd = header.find("\r\n", contentTypeBegin);
+                contentType = stripLeadingAndTrailingHTTPSpaces(header.substring(contentTypeBegin + contentTypePrefixLength, contentTypeEnd - contentTypeBegin - contentTypePrefixLength));
+            }
+
+            form.append(name, File::create(Blob::create(SharedBuffer::create(bodyBegin, bodyLength).get(), Blob::normalizedContentType(contentType)).get(), filename).get(), filename);
+        }
+        return true;
+    };
+    
+    auto parseMultipartBoundary = [] (const Optional<MimeType>& mimeType) -> Optional<String> {
+        if (!mimeType)
+            return WTF::nullopt;
+        if (equalIgnoringASCIICase(mimeType->type, "multipart") && equalIgnoringASCIICase(mimeType->subtype, "form-data")) {
+            auto iterator = mimeType->parameters.find("boundary"_s);
+            if (iterator != mimeType->parameters.end())
+                return iterator->value;
+        }
+        return WTF::nullopt;
+    };
+
+    auto form = DOMFormData::create(UTF8Encoding());
+    auto mimeType = parseMIMEType(contentType);
+    if (auto multipartBoundary = parseMultipartBoundary(mimeType)) {
+        String boundaryWithDashes = makeString("--", *multipartBoundary);
+        const char* boundary = boundaryWithDashes.utf8().data();
+        size_t boundaryLength = strlen(boundary);
+
+        const char* currentBoundary = static_cast<const char*>(memmem(data, length, boundary, boundaryLength));
+        if (!currentBoundary)
+            return nullptr;
+        const char* nextBoundary = static_cast<const char*>(memmem(currentBoundary + boundaryLength, length - (currentBoundary + boundaryLength - data), boundary, boundaryLength));
+        if (!nextBoundary)
+            return nullptr;
+        while (nextBoundary) {
+            parseMultipartPart(currentBoundary + boundaryLength, nextBoundary - currentBoundary - boundaryLength - strlen("\r\n"), form.get());
+            currentBoundary = nextBoundary;
+            nextBoundary = static_cast<const char*>(memmem(nextBoundary + boundaryLength, length - (nextBoundary + boundaryLength - data), boundary, boundaryLength));
+        }
+    } else if (mimeType && equalIgnoringASCIICase(mimeType->type, "application") && equalIgnoringASCIICase(mimeType->subtype, "x-www-form-urlencoded")) {
+        auto dataString = String::fromUTF8(data, length);
+        for (auto& pair : WTF::URLParser::parseURLEncodedForm(dataString))
+            form->append(pair.key, pair.value);
+    } else
+        return nullptr;
+
+    return form;
 }
 
 static void resolveWithTypeAndData(Ref<DeferredPromise>&& promise, FetchBodyConsumer::Type type, const String& contentType, const unsigned char* data, unsigned length)
@@ -59,6 +228,12 @@ static void resolveWithTypeAndData(Ref<DeferredPromise>&& promise, FetchBodyCons
     case FetchBodyConsumer::Type::Text:
         promise->resolve<IDLDOMString>(TextResourceDecoder::textFromUTF8(data, length));
         return;
+    case FetchBodyConsumer::Type::FormData:
+        if (auto formData = packageFormData(contentType, reinterpret_cast<const char*>(data), length))
+            promise->resolve<IDLInterface<DOMFormData>>(*formData);
+        else
+            promise->reject(TypeError);
+        return;
     case FetchBodyConsumer::Type::None:
         ASSERT_NOT_REACHED();
         return;
@@ -75,9 +250,9 @@ void FetchBodyConsumer::clean()
     }
 }
 
-void FetchBodyConsumer::resolveWithData(Ref<DeferredPromise>&& promise, const unsigned char* data, unsigned length)
+void FetchBodyConsumer::resolveWithData(Ref<DeferredPromise>&& promise, const String& contentType, const unsigned char* data, unsigned length)
 {
-    resolveWithTypeAndData(WTFMove(promise), m_type, m_contentType, data, length);
+    resolveWithTypeAndData(WTFMove(promise), m_type, contentType, data, length);
 }
 
 void FetchBodyConsumer::extract(ReadableStream& stream, ReadableStreamToSharedBufferSink::Callback&& callback)
@@ -87,11 +262,11 @@ void FetchBodyConsumer::extract(ReadableStream& stream, ReadableStreamToSharedBu
     m_sink->pipeFrom(stream);
 }
 
-void FetchBodyConsumer::resolve(Ref<DeferredPromise>&& promise, ReadableStream* stream)
+void FetchBodyConsumer::resolve(Ref<DeferredPromise>&& promise, const String& contentType, ReadableStream* stream)
 {
     if (stream) {
         ASSERT(!m_sink);
-        m_sink = ReadableStreamToSharedBufferSink::create([promise = WTFMove(promise), data = SharedBuffer::create(), type = m_type, contentType = m_contentType](auto&& result) mutable {
+        m_sink = ReadableStreamToSharedBufferSink::create([promise = WTFMove(promise), data = SharedBuffer::create(), type = m_type, contentType](auto&& result) mutable {
             if (result.hasException()) {
                 promise->reject(result.releaseException());
                 return;
@@ -127,6 +302,14 @@ void FetchBodyConsumer::resolve(Ref<DeferredPromise>&& promise, ReadableStream* 
     case Type::Text:
         promise->resolve<IDLDOMString>(takeAsText());
         return;
+    case FetchBodyConsumer::Type::FormData: {
+        auto buffer = takeData();
+        if (auto formData = packageFormData(contentType, buffer ? buffer->data() : nullptr, buffer ? buffer->size() : 0))
+            promise->resolve<IDLInterface<DOMFormData>>(*formData);
+        else
+            promise->reject(TypeError);
+        return;
+    }
     case Type::None:
         ASSERT_NOT_REACHED();
         return;
@@ -169,7 +352,7 @@ RefPtr<JSC::ArrayBuffer> FetchBodyConsumer::takeAsArrayBuffer()
 Ref<Blob> FetchBodyConsumer::takeAsBlob()
 {
     if (!m_buffer)
-        return Blob::create(Vector<uint8_t>(), m_contentType);
+        return Blob::create(Vector<uint8_t>(), Blob::normalizedContentType(m_contentType));
 
     // FIXME: We should try to move m_buffer to Blob without doing extra copy.
     return blobFromData(reinterpret_cast<const unsigned char*>(m_buffer->data()), m_buffer->size(), m_contentType);
@@ -221,16 +404,16 @@ void FetchBodyConsumer::loadingFailed(const Exception& exception)
     }
 }
 
-void FetchBodyConsumer::loadingSucceeded()
+void FetchBodyConsumer::loadingSucceeded(const String& contentType)
 {
     m_isLoading = false;
 
     if (m_consumePromise) {
         if (!m_userGestureToken || m_userGestureToken->hasExpired(UserGestureToken::maximumIntervalForUserGestureForwardingForFetch()) || !m_userGestureToken->processingUserGesture())
-            resolve(m_consumePromise.releaseNonNull(), nullptr);
+            resolve(m_consumePromise.releaseNonNull(), contentType, nullptr);
         else {
             UserGestureIndicator gestureIndicator(m_userGestureToken, UserGestureToken::GestureScope::MediaOnly, UserGestureToken::IsPropagatedFromFetch::Yes);
-            resolve(m_consumePromise.releaseNonNull(), nullptr);
+            resolve(m_consumePromise.releaseNonNull(), contentType, nullptr);
         }
     }
     if (m_source) {

LayoutTests/imported/w3c/web-platform-tests/fetch/api/abort/general.any-expected.txt
@@ -38,7 +38,7 @@ PASS Underlying connection is closed when aborting after receiving response
 PASS Underlying connection is closed when aborting after receiving response - no-cors 
 PASS Fetch aborted & connection closed when aborted after calling response.arrayBuffer() 
 PASS Fetch aborted & connection closed when aborted after calling response.blob() 
-FAIL Fetch aborted & connection closed when aborted after calling response.formData() promise_rejects_dom: function "function () { throw e }" threw object "NotSupportedError: The operation is not supported." that is not a DOMException AbortError: property "code" is equal to 9, expected 20
+PASS Fetch aborted & connection closed when aborted after calling response.formData() 
 PASS Fetch aborted & connection closed when aborted after calling response.json() 
 PASS Fetch aborted & connection closed when aborted after calling response.text() 
 PASS Stream errors once aborted. Underlying connection closed. 

LayoutTests/imported/w3c/web-platform-tests/fetch/api/abort/general.any.worker-expected.txt
@@ -38,7 +38,7 @@ PASS Underlying connection is closed when aborting after receiving response
 PASS Underlying connection is closed when aborting after receiving response - no-cors 
 PASS Fetch aborted & connection closed when aborted after calling response.arrayBuffer() 
 PASS Fetch aborted & connection closed when aborted after calling response.blob() 
-FAIL Fetch aborted & connection closed when aborted after calling response.formData() promise_rejects_dom: function "function () { throw e }" threw object "NotSupportedError: The operation is not supported." that is not a DOMException AbortError: property "code" is equal to 9, expected 20
+PASS Fetch aborted & connection closed when aborted after calling response.formData() 
 PASS Fetch aborted & connection closed when aborted after calling response.json() 
 PASS Fetch aborted & connection closed when aborted after calling response.text() 
 PASS Stream errors once aborted. Underlying connection closed. 

LayoutTests/imported/w3c/web-platform-tests/fetch/api/request/request-consume-empty-expected.txt
@@ -11,6 +11,6 @@ PASS Consume empty text request body as arrayBuffer
 PASS Consume empty blob request body as text 
 PASS Consume empty text request body as text 
 PASS Consume empty URLSearchParams request body as text 
-FAIL Consume empty FormData request body as text promise_test: Unhandled rejection with value: object "NotSupportedError: The operation is not supported."
+FAIL Consume empty FormData request body as text assert_equals: Resolved value should be empty expected 0 but got 44
 PASS Consume empty ArrayBuffer request body as text 
 

LayoutTests/imported/w3c/web-platform-tests/fetch/api/request/request-consume-expected.txt
@@ -23,7 +23,7 @@ PASS Consume DataView request's body as text
 PASS Consume DataView request's body as blob 
 PASS Consume DataView request's body as arrayBuffer 
 PASS Consume DataView request's body as JSON 
-FAIL Consume FormData request's body as FormData promise_test: Unhandled rejection with value: object "NotSupportedError: The operation is not supported."
+PASS Consume FormData request's body as FormData 
 PASS Consume blob response's body as blob 
 PASS Consume blob response's body as text 
 PASS Consume blob response's body as json 

LayoutTests/imported/w3c/web-platform-tests/fetch/api/request/request-init-002-expected.txt
@@ -3,7 +3,7 @@ PASS Initialize Request with headers values
 PASS Initialize Request's body with "undefined", undefined 
 PASS Initialize Request's body with "null", null 
 PASS Initialize Request's body with "[object Blob]", application/octet-binary 
-FAIL Initialize Request's body with "[object FormData]", multipart/form-data promise_test: Unhandled rejection with value: object "NotSupportedError: The operation is not supported."
+PASS Initialize Request's body with "[object FormData]", multipart/form-data 
 PASS Initialize Request's body with "This is a USVString", text/plain;charset=UTF-8 
 PASS Initialize Request's body with "hi!", text/plain;charset=UTF-8 
 PASS Initialize Request's body with "name=value", application/x-www-form-urlencoded;charset=UTF-8 

LayoutTests/imported/w3c/web-platform-tests/fetch/api/response/response-consume-empty-expected.txt
@@ -11,6 +11,6 @@ PASS Consume empty text response body as arrayBuffer
 PASS Consume empty blob response body as text 
 PASS Consume empty text response body as text 
 PASS Consume empty URLSearchParams response body as text 
-FAIL Consume empty FormData response body as text promise_test: Unhandled rejection with value: object "NotSupportedError: The operation is not supported."
+FAIL Consume empty FormData response body as text assert_equals: Resolved value should be empty expected 0 but got 44
 PASS Consume empty ArrayBuffer response body as text 
 

commit 0d9f56d0f22d43780da58c58faacf5e79f8e548f

Cherry-pick r266140. rdar://problem/68164582
    Fix read-after-free introduced in r266087
    https://bugs.webkit.org/show_bug.cgi?id=215671

    * Modules/fetch/FetchBodyConsumer.cpp:
    (WebCore::packageFormData):
    Keep the CString in scope while we are using it.

    Canonical link: https://commits.webkit.org/228612@trunk
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@266140 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Canonical link: https://commits.webkit.org/228499.17@safari-610-branch
git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-610-branch@266430 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/Modules/fetch/FetchBodyConsumer.cpp
@@ -187,19 +187,19 @@ static RefPtr<DOMFormData> packageFormData(ScriptExecutionContext* context, cons
     auto mimeType = parseMIMEType(contentType);
     if (auto multipartBoundary = parseMultipartBoundary(mimeType)) {
         String boundaryWithDashes = makeString("--", *multipartBoundary);
-        const char* boundary = boundaryWithDashes.utf8().data();
-        size_t boundaryLength = strlen(boundary);
+        CString boundary = boundaryWithDashes.utf8();
+        size_t boundaryLength = boundary.length();
 
-        const char* currentBoundary = static_cast<const char*>(memmem(data, length, boundary, boundaryLength));
+        const char* currentBoundary = static_cast<const char*>(memmem(data, length, boundary.data(), boundaryLength));
         if (!currentBoundary)
             return nullptr;
-        const char* nextBoundary = static_cast<const char*>(memmem(currentBoundary + boundaryLength, length - (currentBoundary + boundaryLength - data), boundary, boundaryLength));
+        const char* nextBoundary = static_cast<const char*>(memmem(currentBoundary + boundaryLength, length - (currentBoundary + boundaryLength - data), boundary.data(), boundaryLength));
         if (!nextBoundary)
             return nullptr;
         while (nextBoundary) {
             parseMultipartPart(currentBoundary + boundaryLength, nextBoundary - currentBoundary - boundaryLength - strlen("\r\n"), form.get());
             currentBoundary = nextBoundary;
-            nextBoundary = static_cast<const char*>(memmem(nextBoundary + boundaryLength, length - (nextBoundary + boundaryLength - data), boundary, boundaryLength));
+            nextBoundary = static_cast<const char*>(memmem(nextBoundary + boundaryLength, length - (nextBoundary + boundaryLength - data), boundary.data(), boundaryLength));
         }
     } else if (mimeType && equalIgnoringASCIICase(mimeType->type, "application") && equalIgnoringASCIICase(mimeType->subtype, "x-www-form-urlencoded")) {
         auto dataString = String::fromUTF8(data, length);